6ffbbdc20d897d52ea535c2be3aa7e8119aea5df
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2014-12-14 12:22:28 +0000

    Install binary to CMAKE_INSTALL_PREFIX/bin

--- extension/nautilus-admin.py
+++ extension/nautilus-admin.py
@@ -3,13 +3,17 @@ import urllib
 
 from gi.repository import Nautilus, GObject, GConf
 
+#TODO: don't fail on paths with spaces
+#TODO: show a warning on first use
+#TODO: localization
+
 class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
 	def __init__(self):
 		self.client = GConf.Client.get_default()
 
 	def nautilus_run(self, menu, url):
 		filename = urllib.unquote(url.get_uri()[7:])
-		os.system("/usr/bin/nautilus-pkexec {} &".format(filename))
+		os.system("@CMAKE_INSTALL_PREFIX@/bin/nautilus-pkexec {} &".format(filename))
 
 	def get_file_items(self, window, files):
 		if(len(files) == 1):
