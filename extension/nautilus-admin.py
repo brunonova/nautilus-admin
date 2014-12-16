1c0276b1443224b46d2738896c5f8667d408e4f4
Bruno Nova <brunomb.nova@gmail.com>
2014-12-16 21:57:15 +0000

    Show a warning dialog on first run
    (Not finished yet)

--- extension/nautilus-admin.py
+++ extension/nautilus-admin.py
@@ -1,10 +1,9 @@
 import os, subprocess
-from gi.repository import Nautilus, GObject, GConf
+from gi.repository import Nautilus, GObject, GConf, Gtk, GLib
 from gettext import gettext, locale, bindtextdomain, textdomain
 
-#TODO: show a warning on first use
-
 ROOT_UID=0
+WARNING_DISPLAYED_FILENAME='.nautilus-admin-warning-displayed'
 
 class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
 	def __init__(self):
@@ -43,5 +42,23 @@ class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
 		textdomain('nautilus-admin')
 
 	def _nautilus_run(self, menu, file):
+		conf_dir = GLib.get_user_config_dir()
+		conf_file = os.path.join(conf_dir, WARNING_DISPLAYED_FILENAME)
+		if not os.path.exists(conf_file):
+			dialog = Gtk.MessageDialog(None, 0, Gtk.MessageType.WARNING,
+				                                Gtk.ButtonsType.OK_CANCEL,
+				                                "Open folder as Administrator")
+			msg = gettext('Running the File Manager as Administrator (<i>root</i> user) is <b>dangerous</b>!\n'
+				          'Proceed only if you know what you are doing!')
+			dialog.format_secondary_markup(msg)
+			response = dialog.run()
+			dialog.destroy()
+			if response == Gtk.ResponseType.OK:
+				if not os.path.isdir(conf_dir):
+					os.makedirs(conf_dir)
+				open(conf_file, 'w').close()
+			else:
+				return
+			
 		uri = file.get_uri()
 		subprocess.Popen(['/usr/bin/pkexec', '/usr/bin/nautilus', '--no-desktop', uri])
