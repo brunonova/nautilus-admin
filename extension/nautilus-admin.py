5894ffb214d35937657066dd8f8e7312fb8c870d
Bruno Nova <brunomb.nova@gmail.com>
2014-12-29 20:11:01 +0000

    Remove warning dialog
    Also some other small changes.

--- extension/nautilus-admin.py
+++ extension/nautilus-admin.py
@@ -3,7 +3,6 @@ from gi.repository import Nautilus, GObject, GConf, Gtk, GLib
 from gettext import gettext, locale, bindtextdomain, textdomain
 
 ROOT_UID = 0
-WARNING_DISPLAYED_FILENAME = ".nautilus-admin-warning-displayed"
 
 class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
 	"""Simple Nautilus extension that adds an 'Open as Administrator' item to
@@ -26,7 +25,7 @@ class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
 		self._setup_gettext();
 		item = Nautilus.MenuItem(name="NautilusAdmin::Nautilus",
 		                         label=gettext("Open as Administrator"),
-		                         tip=gettext("Open this folder as administrator"))
+		                         tip=gettext("Open this folder with root privileges"))
 		item.connect("activate", self._nautilus_run, file)
 		return (item, )
 
@@ -43,38 +42,12 @@ class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
 		self._setup_gettext();
 		item = Nautilus.MenuItem(name="NautilusAdmin::Nautilus",
 		                         label=gettext("Open as Administrator"),
-		                         tip=gettext("Open this folder as administrator"))
+		                         tip=gettext("Open this folder with root privileges"))
 		item.connect("activate", self._nautilus_run, file)
 		return (item, )
 
 	def _nautilus_run(self, menu, file):
 		"""'Open as Administrator' menu item callback."""
-		# Show a warning dialog when first executed (by checking the file
-		# WARNING_DISPLAYED_FILENAME in ~/.config)
-		conf_dir = GLib.get_user_config_dir()
-		conf_file = os.path.join(conf_dir, WARNING_DISPLAYED_FILENAME)
-		if not os.path.exists(conf_file):
-			dialog = Gtk.MessageDialog(None, 0, Gtk.MessageType.WARNING,
-				                                Gtk.ButtonsType.OK_CANCEL,
-				                                "Open folder as Administrator")
-			msg = gettext("Running the File Manager as Administrator (<i>root</i> user) is <b>dangerous</b>!\n"
-				          "Proceed only if you know what you are doing!")
-			dialog.format_secondary_markup(msg)
-			response = dialog.run()
-			dialog.destroy()
-
-			# Create WARNING_DISPLAYED_FILENAME to mark the dialog as displayed
-			if response == Gtk.ResponseType.OK:
-				try:
-					if not os.path.isdir(conf_dir):
-						os.makedirs(conf_dir)
-					open(conf_file, "w").close() # create an empty file
-				except:
-					pass
-			else:
-				return
-
-		# Open file in Nautilus as root
 		uri = file.get_uri()
 		subprocess.Popen(["/usr/bin/pkexec", "/usr/bin/nautilus", "--no-desktop", uri])
 
