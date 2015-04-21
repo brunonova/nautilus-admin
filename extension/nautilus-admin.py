2815808e23028c2c391a9487fcc0b0293363ece7
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2015-04-21 17:01:11 +0100

    Refactoring

--- extension/nautilus-admin.py
+++ extension/nautilus-admin.py
@@ -5,54 +5,58 @@ from gettext import gettext, locale, bindtextdomain, textdomain
 ROOT_UID = 0
 
 class NautilusAdmin(Nautilus.MenuProvider, GObject.GObject):
-	"""Simple Nautilus extension that adds an 'Open as Administrator' item to
-	the right-click menu, which opens the selected folder as root in Nautilus
-	using 'pkexec'."""
+	"""Simple Nautilus extension that adds some administrative (root) actions to
+	the right-click menu, using 'pkexec' to authenticate the administrator."""
 	def __init__(self):
 		pass
 
 	def get_file_items(self, window, files):
+		"""Returns the menu items to display when one or more files/folders are
+		selected."""
 		# Don't show when already running as root, or when more than 1 file is selected
 		if os.geteuid() == ROOT_UID or len(files) != 1:
 			return
 		file = files[0]
 
-		# file must be a local directory
-		if not file.is_directory() or file.get_uri_scheme() != "file":
-			return
-
-		# Create the menu item
+		# Add the menu items
+		items = []
 		self._setup_gettext();
-		item = Nautilus.MenuItem(name="NautilusAdmin::Nautilus",
-		                         label=gettext("Open as Administrator"),
-		                         tip=gettext("Open this folder with root privileges"))
-		item.connect("activate", self._nautilus_run, file)
-		return (item, )
+		if file.is_directory() and file.get_uri_scheme() == "file":
+			items += [self._create_open_as_admin(file)]
+
+		return items
 
 	def get_background_items(self, window, file):
+		"""Returns the menu items to display when no file/folder is selected
+		(i.e. when right-clicking the background)."""
 		# Don't show when already running as root
 		if os.geteuid() == ROOT_UID:
 			return
 
-		# file must be a local directory
-		if not file.is_directory() or file.get_uri_scheme() != "file":
-			return
-
-		# Create the menu item
+		# Add the menu items
+		items = []
 		self._setup_gettext();
-		item = Nautilus.MenuItem(name="NautilusAdmin::Nautilus",
-		                         label=gettext("Open as Administrator"),
-		                         tip=gettext("Open this folder with root privileges"))
-		item.connect("activate", self._nautilus_run, file)
-		return (item, )
+		if file.is_directory() and file.get_uri_scheme() == "file":
+			items += [self._create_open_as_admin(file)]
 
-	def _nautilus_run(self, menu, file):
-		"""'Open as Administrator' menu item callback."""
-		uri = file.get_uri()
-		subprocess.Popen(["/usr/bin/pkexec", "/usr/bin/nautilus", "--no-desktop", uri])
+		return items
 
 	def _setup_gettext(self):
 		"""Initializes gettext to localize strings."""
 		locale.setlocale(locale.LC_ALL, "")
 		bindtextdomain("nautilus-admin", "@CMAKE_INSTALL_PREFIX@/share/locale")
 		textdomain("nautilus-admin")
+
+	def _create_open_as_admin(self, file):
+		"""Creates the 'Open as Administrator' menu item."""
+		item = Nautilus.MenuItem(name="NautilusAdmin::Nautilus",
+		                         label=gettext("Open as Administrator"),
+		                         tip=gettext("Open this folder with root privileges"))
+		item.connect("activate", self._nautilus_run, file)
+		return item
+
+
+	def _nautilus_run(self, menu, file):
+		"""'Open as Administrator' menu item callback."""
+		uri = file.get_uri()
+		subprocess.Popen(["/usr/bin/pkexec", "/usr/bin/nautilus", "--no-desktop", uri])
