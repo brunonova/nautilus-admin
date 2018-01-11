02fa761f34d8b9691a7bc0df052e3f60f146a25a
Dzen Python <30671470+zgwerby@users.noreply.github.com>
2018-01-11 09:24:25 +0300

    Update nautilus-admin.py

--- extension/nautilus-admin.py
+++ extension/nautilus-admin.py
@@ -17,12 +17,6 @@
 
 import os, subprocess
 
-try:
-    from urlparse import urlparse
-except ImportError:
-    from urllib.parse import urlparse
-
-
 from gi import require_version
 require_version('Gtk', '3.0')
 require_version('Nautilus', '3.0')
